f85e574f948eb0ed828e2abe6ee2872162bd4b9d
wangkun <wangkun@woodemi.com>
2020-03-12 08:05:49 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ lib/sample.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class Sample {
+  static const MethodChannel _channel = const MethodChannel('sample');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
